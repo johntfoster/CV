a48f7ff9de4cc0494748a1090ed2368856b859b9
John Foster <john.foster@utsa.edu>
2014-09-10 10:18:06 -0500

    Update travis_deploy

--- .utilities/travis_deploy.sh
+++ .utilities/travis_deploy.sh
@@ -1,3 +1,3 @@
 # Use Travis API to rebuild johntfoster/johntfoter-professional-website
-travis login --org --github-token ${GH_TOKEN}
-travis restart -r johntfoster/johntfoster-professional-website
+travis login --org --github-token ${GH_TOKEN} --skip-completion-check
+travis restart -r johntfoster/johntfoster-professional-website --skip-completion-check
