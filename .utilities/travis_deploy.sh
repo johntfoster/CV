39776774531b11471aa68666ff164bac951bd2c5
John Foster <john.foster@utsa.edu>
2014-09-10 15:35:37 -0500

    Update Travis

--- .utilities/travis_deploy.sh
+++ .utilities/travis_deploy.sh
@@ -1,4 +1,10 @@
-# Use Travis API to rebuild johntfoster/johntfoter-professional-website
-travis login --org --github-token ${GH_TOKEN} --skip-completion-check
-export TRAVIS_TOKEN=`travis token | grep -v "Your Travis token is "`
-travis restart -r johntfoster/johntfoster-professional-website --skip-completion-check --token ${TRAVIS_TOKEN}
+# Get to the Travis build directory, configure git and clone the repo
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+
+# Commit and Push the Changes
+git remote add temp https://${GH_TOKEN}@github.com/johntfoster/CV.git > /dev/null
+git add cv.pdf
+git commit -m "Travis CI autocommit from travis build $TRAVIS_BUILD_NUMBER"
+git push -fq temp master > /dev/null
