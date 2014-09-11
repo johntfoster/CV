a3f8f8e45bed7e387d405f2cd35ec2fba424e878
John Foster <john.foster@utsa.edu>
2014-09-11 08:46:23 -0500

    Update Travis Deploy

--- .utilities/travis_deploy.sh
+++ .utilities/travis_deploy.sh
@@ -1,8 +1,12 @@
-# Get to the Travis build directory, configure git and clone the repo
+#!/usr/bin/env bash
+
+#Configure git and clone the repo
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" >> .git/credentials
 
 # Commit and Push the Changes
 git add cv.pdf
 git commit -m "Travis CI autocommit from travis build $TRAVIS_BUILD_NUMBER"
-git push -fq https://${GH_TOKEN}@github.com/johntfoster/CV.git master
+git push -fq origin master
