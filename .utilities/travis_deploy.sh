279dc1d303d6c950a86ef5b256b296183d88539a
John Foster <john.foster@utsa.edu>
2014-09-12 08:34:35 -0500

    Adding html build support

--- .utilities/travis_deploy.sh
+++ .utilities/travis_deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 #Configure git and clone the repo
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" >> .git/credentials
 
 # Commit and Push the Changes
 git checkout master
 git remote set-url origin "https://${GH_TOKEN}@github.com/johntfoster/CV.git"
-git add cv.pdf
-git commit -m "Travis CI autocommit from travis build $TRAVIS_BUILD_NUMBER [ci skip]"
+git add cv.pdf cv.html
+git commit -m "Travis CI autocommit from travis build $TRAVIS_BUILD_NUMBER \n\n [ci skip]"
 git push -fq origin master
