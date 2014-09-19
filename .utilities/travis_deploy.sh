0c3ff5009f48860edcc1830fa6e3766cc89c06e1
John Foster <john.foster@utsa.edu>
2014-09-19 13:14:11 -0500

    Debug Travis push to travis-build branch

--- .utilities/travis_deploy.sh
+++ .utilities/travis_deploy.sh
@@ -7,10 +7,10 @@ git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" >> .git/credentials
 
 # Commit and Push the Changes
-git clone --quiet --branch=travis-build https://${GH_TOKEN}@github.com/johntfoster/johntfoster-professional-website travis-build > /dev/null
+git clone --quiet --branch=travis-build https://${GH_TOKEN}@github.com/johntfoster/CV travis-build > /dev/null
 cd travis-build
-cp $HOME/build/johntfoster/CV/cv.pdf .
-cp $HOME/build/johntfoster/CV/cv.html .
-git add -A
+cp -f $HOME/build/johntfoster/CV/cv.pdf .
+cp -f $HOME/build/johntfoster/CV/cv.html .
+git add cv.pdf cv.html
 git commit -m "Travis CI autocommit from travis build $TRAVIS_BUILD_NUMBER [ci skip]"
 git push -fq origin travis-build > /dev/null
